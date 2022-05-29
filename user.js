163fcae820ca0ab36f7b1fade2727f694311ed8a
Jakub Trzeciak <jptrzy@gmail.com>
2022-05-29 18:10:58 +0200

    :sparkles: Install script

---
+++ user.js
@@ -0,0 +1,13 @@
+
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+user_pref("browser.urlbar.suggest.openpage", false);
+user_pref("browser.urlbar.openViewOnFocus", false);
+user_pref("browser.urlbar.update1", false);
+user_pref("browser.urlbar.update1.interventions", false);
+user_pref("browser.urlbar.update1.searchTips", false);
+
+user_pref("browser.urlbar.disableExtendForTests", true);
+user_pref("browser.urlbar.maxRichResults", 0);
+
+user_pref("ui.prefersReducedMotion", 1);
